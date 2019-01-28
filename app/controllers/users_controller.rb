ff9a7645a619ddd8a4833c50c3db01ebaf18defe
Jacger <quangjacger99@gmail.com>
2019-01-28 09:52:32 +0700

    chapter_8

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,20 +7,21 @@ class UsersController < ApplicationController
     @user = User.new user_params
 
     if @user.save
-      redirect_to action: :show, id: @user.id
-      flash[:success] = t(:succes_to_create_user)
+      log_in @user
+      redirect_to @user
+      flash[:success] = t :success_to_create_user
     else
       render :new
-      flash[:danger] = t(:fail_to_create_user)
+      flash[:danger] = t :fail_to_create_user
     end
   end
 
   def show
     @user = User.find_by id: params[:id]
-    
+
     return if @user
-    redirect_to action: :new
-    flash[:danger] = t(:fail_to_load_user)
+    redirect_to :new
+    flash[:danger] = t :fail_to_load_user
   end
 
   private
